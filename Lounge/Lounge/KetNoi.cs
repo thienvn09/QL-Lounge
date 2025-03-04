ec3ffc580d02f4a348b0212faa4a43e6f4c20f9b
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-03-05 01:58:48 +0700

    Update KetNoi.cs

--- Lounge/Lounge/KetNoi.cs
+++ Lounge/Lounge/KetNoi.cs
@@ -8,6 +8,23 @@ namespace Lounge
 {
     internal class KetNoi
     {
-
+        string sql ="LAPTOP-HCQ7FVIS\SQLEXPRESS;Initial Catalog=QLNhaHang;Integrated Security=True";
+        public SqlConnection conn = null;
+        public void MoKetNoi()
+        {
+            conn = new SqlConnection(sql);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+        }
+        public void DongKetNoi()
+        {
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+        
     }
 }
