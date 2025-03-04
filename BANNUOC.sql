57ffb8eefcc65cda6060fab23d7131b985e320d3
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-03-05 00:42:09 +0700

    Update BANNUOC.sql

--- BANNUOC.sql
+++ BANNUOC.sql
@@ -28,11 +28,12 @@ CREATE TABLE DangNhap(
 	TrangThai NVARCHAR(20) NOT NULL CHECK(TrangThai IN('Hoạt động','Khóa')),
 	FOREIGN KEY (MaNV) REFERENCES NhanVien(MaNV) ON DELETE CASCADE
 );
-CREATE TABLE DanhMucSanPham(
-	MaDanhMuc INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-	TenDanhMuc NVARCHAR(100) NOT Null,
-	Loai NVARCHAR(20) NOT NULL Check(Loai In('Đồ uống','Món ăn')),
-	MoTa NVARCHAR(200) NULL,
+CREATE TABLE DanhMucSanPham (
+    MaDanhMuc INT PRIMARY KEY IDENTITY(1,1),
+    TenDanhMuc NVARCHAR(100) NOT NULL,
+    Loai NVARCHAR(20) NOT NULL CHECK (Loai IN ('Đồ uống', 'Món ăn')), -- Loại sản phẩm
+    ThueVAT DECIMAL(5, 2) NOT NULL CHECK (ThueVAT IN (8.00, 10.00)), -- Thuế VAT (8% hoặc 10%)
+    MoTa NVARCHAR(MAX)
 );
 CREATE TABLE NhaCungCap(
 	MaNhaCungCap INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
@@ -46,15 +47,17 @@ CREATE TABLE SANPHAM(
 	MaDanhMuc INT, -- mối quan hệ n-1 mới bảng danh mục sp
 	MaNhaCungCap INT, -- Mối quan hệ n-1 với bảng nhà cung cấp
 	Gia DECIMAL(10,2) NOT NULL,
-	ThueVAT AS(
-		CASE
-			WHEN MaDanhMuc = (SELECT MaDanhMuc FROM DanhMucSanPham WHERE TenDanhMuc ='Đồ uống có cồn')
-			 Then 10.00
-			 ELSE 8.00
-			 END
-			 ),
 	MoTa NVARCHAR(200) NULL,
 	SoLuongTonKho INT NOT NULL DEFAULT 0,
 	FOREIGN KEY (MaDanhMuc) REFERENCES DanhMucSanPham(MaDanhMuc),
 	FOREIGN KEY (MaNhaCungCap) REFERENCES NhaCungCap(MaNhaCungCap),
 );
+CREATE TABLE HoaDon(
+	MaHoaDon INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	MaKhachHang INT  NOT NULL, -- tham chiếu tới bảng khách hàng
+	MaNhanVien INT, -- tham chiếu tới bảng nhân viên
+	MaBan INT, -- tham chiếu tới bảng bàn
+	NgayDat DATETIME NOT NULL DEFAULT GETDATE(),
+	TongTien DECIMAL(10, 2), -- Tổng tiền trước thuế và giảm giá
+	TiemGiamGia DECIMAL(10,2) DEFAULT 0.00,
+	TongThue DECIMAL (10,2), -- Tổng tiền thuế
