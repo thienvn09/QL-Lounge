601db3aec9090b2dcb723414906a958b74a37350
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-03-15 15:22:47 +0700

    zzz

---
+++ DoAnNho/DoAnNho/Program.cs
@@ -0,0 +1,18 @@
+﻿using DoAnNho.PL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnNho
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LoginFormPL formMain = new LoginFormPL();
+            formMain.ShowDialog();
+        }
+    }
+}
