850d85e3aabf25a55e429d7e33b1b2418bcb2d2e
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-03-21 22:36:04 +0700

    Update BANNUOC.sql

--- HQTCSDL/BANNUOC.sql
+++ HQTCSDL/BANNUOC.sql
@@ -2,7 +2,6 @@ CREATE DATABASE QL_NHAHANG
 GO
 USE QL_NHAHANG
 GO
-
 CREATE TABLE KhachHang(
 	MaKhachHang INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
 	HoTen NVARCHAR(100) NOT NULL,
@@ -29,11 +28,12 @@ CREATE TABLE DangNhap(
 	TrangThai NVARCHAR(20) NOT NULL CHECK(TrangThai IN('Hoạt động','Khóa')),
 	FOREIGN KEY (MaNV) REFERENCES NhanVien(MaNV) ON DELETE CASCADE
 );
-CREATE TABLE DanhMucSanPham(
-	MaDanhMuc INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-	TenDanhMuc NVARCHAR(100) NOT Null,
-	Loai NVARCHAR(20) NOT NULL Check(Loai In('Đồ uống','Món ăn')),
-	MoTa NVARCHAR(200) NULL,
+CREATE TABLE DanhMucSanPham (
+    MaDanhMuc INT PRIMARY KEY IDENTITY(1,1),
+    TenDanhMuc NVARCHAR(100) NOT NULL,
+    Loai NVARCHAR(20) NOT NULL CHECK (Loai IN ('Đồ uống', 'Món ăn')), -- Loại sản phẩm
+    ThueVAT DECIMAL(5, 2) NOT NULL CHECK (ThueVAT IN (8.00, 10.00)), -- Thuế VAT (8% hoặc 10%)
+    MoTa NVARCHAR(MAX)
 );
 CREATE TABLE NhaCungCap(
 	MaNhaCungCap INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
@@ -47,15 +47,115 @@ CREATE TABLE SANPHAM(
 	MaDanhMuc INT, -- mối quan hệ n-1 mới bảng danh mục sp
 	MaNhaCungCap INT, -- Mối quan hệ n-1 với bảng nhà cung cấp
 	Gia DECIMAL(10,2) NOT NULL,
-	ThueVAT AS(
-		CASE
-			WHEN MaDanhMuc = (SELECT MaDanhMuc FROM DanhMuc WHERE TenDanhMuc ='Đồ uống có cồn')
-			 Then 10.00
-			 ELSE 8.00
-			 END
-			 ),
 	MoTa NVARCHAR(200) NULL,
 	SoLuongTonKho INT NOT NULL DEFAULT 0,
 	FOREIGN KEY (MaDanhMuc) REFERENCES DanhMucSanPham(MaDanhMuc),
 	FOREIGN KEY (MaNhaCungCap) REFERENCES NhaCungCap(MaNhaCungCap),
 );
+CREATE TABLE Ban(
+	MaBan INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	SoBan NVARCHAR(40) NOT NULL,
+	SoChoNgoi INT NOT NULL,
+	KhuVuc NVARCHAR(20) NOT NULL CHECK( KhuVuc In('Nhà Hàng','Quầy Bar')),
+	TrangThai NVARCHAR(20) NOT NULL CHECK( TrangThai IN('Trống','Đang sử dụng')),
+);
+CREATE TABLE Voucher(
+	MaVoucher INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	MaKhachHang INT, -- tham chiếu tới bảng khách hàng
+	MaSanPham INT, -- tham chiếu tới bảng sản phẩm
+	GiaTri DECIMAL(10,2) NOT NULL,
+	NgayHetHan DATE,
+	TrangThai NVARCHAR(20) NOT NULL CHECK(TrangThai IN('Đã sử dụng' , 'Chưa sử dụng')),
+	FOREIGN KEY (MaKhachHang) REFERENCES KhachHang (MaKhachHang),
+    FOREIGN KEY (MaSanPham) REFERENCES SanPham(MaSanPham)
+);
+CREATE TABLE HoaDon(
+	MaHoaDon INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	MaKhachHang INT  NOT NULL, -- tham chiếu tới bảng khách hàng
+	MaNhanVien INT, -- tham chiếu tới bảng nhân viên
+	MaBan INT, -- tham chiếu tới bảng bàn
+	NgayDat DATETIME NOT NULL DEFAULT GETDATE(),
+	TongTien DECIMAL(10, 2), -- Tổng tiền trước thuế và giảm giá
+	TienGiamGia DECIMAL(10,2) DEFAULT 0.00,
+	TongThueVAT DECIMAL (10,2), -- Tổng tiền thuế
+	ThanhToan AS (TongTien + TongThueVAT - TienGiamGia),
+	TrangThai NVARCHAR(20) DEFAULT 'Chưa thanh toán' CHECK (TrangThai IN ('Chưa thanh toán', 'Đã thanh toán')),
+	NguoiTao INT,
+	FOREIGN KEY (MaKhachHang) REFERENCES KhachHang(MaKhachHang),
+    FOREIGN KEY (MaNhanVien) REFERENCES NhanVien(MaNV),
+    FOREIGN KEY (MaBan) REFERENCES Ban(MaBan),
+    FOREIGN KEY (NguoiTao) REFERENCES NhanVien(MaNV),
+);
+CREATE TABLE ChiTietHoaDon(
+	MaChiTietHoaDon INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	MaHoaDon INT, -- tham chiếu tới bảng hóa đơn
+	MaSanPham INT, -- tham chiếu tới bảng sản phẩm
+	SoLuong INT NOT NULL,
+	Gia DECIMAL(10,2) NOT NULL, -- giá của sản phẩm 
+	ThueVAT DECIMAL(5,2) NOT NULL , -- lấy từ danh mục sản phẩm 
+	ThanhTien AS(Gia *SoLuong),
+	TienThue AS (Gia * SoLuong * ThueVAT /100), --tiền thuế (thành tiền * thuế vat /100)
+	FOREIGN KEY (MaHoaDon) REFERENCES HoaDon(MaHoaDon),
+	FOREIGN KEY (MaSanPham) REFERENCES SanPham(MaSanPham),
+);
+CREATE TABLE KhoHang(
+	MaKhoHang INT PRIMARY KEY IDENTITY(1,1),
+    MaSanPham INT, -- Tham chiếu đến bảng SanPham
+    SoLuongTon INT NOT NULL DEFAULT 0, -- Số lượng tồn kho
+    FOREIGN KEY (MaSanPham) REFERENCES SanPham(MaSanPham)
+);
+CREATE TABLE PhieuNhapKho(
+	MaPhieuNhap INT PRIMARY KEY IDENTITY(1,1),
+	NgayNhap Date Not NULL,
+	MaNV INT NOT NULL, --tham chiếu tới bảng nhân viên
+	TongTienNhap DECIMAL(15,2) NOT NULL,
+	GhiChu NVARCHAR(MAX),
+	FOREIGN KEY (MaNV) REFERENCES NhanVien(MaNV),
+
+);
+CREATE TABLE ChiTietPhieuNhapKho(
+	MaChiTietPhieuNhap INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	MaPhieuNhap INT not null, -- tham chiếu tới bang phiếu nhập
+	MaSanPham INT not null,
+	SoLuong INT Not Null, -- số lượng nhập
+	DonGia DECIMAL(10, 2) NOT NULL, -- Đơn giá nhập
+	ThanhTien AS (SoLuong * DonGia), -- Thành tiền tự động tính
+    FOREIGN KEY (MaPhieuNhap) REFERENCES PhieuNhapKho(MaPhieuNhap),
+    FOREIGN KEY (MaSanPham) REFERENCES SanPham(MaSanPham)
+);
+CREATE TABLE PhieuXuatKho (
+    MaPhieuXuat INT PRIMARY KEY IDENTITY(1,1),
+    NgayXuat DATE NOT NULL DEFAULT GETDATE(), -- Ngày xuất kho
+    MaNhanVien INT, -- Người thực hiện xuất kho (tham chiếu đến bảng NhanVien)
+    GhiChu NVARCHAR(MAX),
+    FOREIGN KEY (MaNhanVien) REFERENCES NhanVien(MaNV)
+);
+CREATE TABLE ChiTietPhieuXuat (
+    MaChiTietXuat INT PRIMARY KEY IDENTITY(1,1),
+    MaPhieuXuat INT, -- Tham chiếu đến bảng PhieuXuatKho
+    MaSanPham INT, -- Tham chiếu đến bảng SanPham
+    SoLuong INT NOT NULL, -- Số lượng xuất
+    FOREIGN KEY (MaPhieuXuat) REFERENCES PhieuXuatKho(MaPhieuXuat),
+    FOREIGN KEY (MaSanPham) REFERENCES SanPham(MaSanPham)
+);
+CREATE TABLE BaoCao(
+	MaBaoCao INT PRIMARY KEY IDENTITY(1,1),
+	LoaiBaoCao NVARCHAR(20) NOT NULL Check( LoaiBaoCao In('Hàng Ngày','Hàng Tháng','Hàng năm')),
+	NgayBaoCao DATE NOT NULL,
+	TongDoanhThu DECIMAL(15,2) NOT NULL,-- Tổng doanh thu
+	TongChiPhi DECIMAL(15,2) NOT Null,
+	LoiNhuan AS(TongDoanhThu - TongChiPhi),
+	SoHoaDon INT NOT NULL,
+	SoKhachHang INT NOT NULL,
+	SoSanPhamBanRa INT NOT NULL,
+	NguoiTao INT, 
+	GhiChu Nvarchar(MAX) Not NULL,
+	FOREIGN KEY (NguoiTao) REFERENCES NhanVien(MaNV),
+);
+
+Use QL_NHAHANG 
+go
+SELECT COUNT(*) AS SoLuongBang
+FROM sys.objects
+WHERE type = 'U'; -- 'U' là loại User Table
+
